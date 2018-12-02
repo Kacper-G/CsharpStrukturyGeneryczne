c8ee98f5a0848a61525d53ffab4a0ad7e21e3125
Kacper-G <kacper.glugla@o2.pl>
2018-12-02 14:00:12 +0100

    2_4 Kolejka

--- CsharpStrukturyGenertczne/1_TypyGeneryczne/Program.cs
+++ CsharpStrukturyGenertczne/1_TypyGeneryczne/Program.cs
@@ -6,31 +6,41 @@ namespace _1_TypyGeneryczne
     {
         static void Main(string[] args)
         {
-            var kolejka = new KolejkaKolowa(pojemnosc: 3);
+            var kolejka = new KolejkaKolowa<double>(pojemnosc: 3);
 
-            while(true)
+            WprowadzanieDanych(kolejka);
+
+            PrzetrwarzanieDanych(kolejka);
+        }
+
+        private static void PrzetrwarzanieDanych(KolejkaKolowa<double> kolejka)
+        {
+            var suma = 0.0;
+            Console.WriteLine("W naszej kolejce jest : ");
+
+            while (!kolejka.JestPusty)
+            {
+                suma += kolejka.Czytaj();
+            }
+            Console.WriteLine("\t\t" + suma);
+            Console.ReadLine();
+        }
+
+        private static void WprowadzanieDanych(KolejkaKolowa<double> kolejka)
+        {
+            while (true)
             {
                 var wartosc = 0.0;
                 var wartoscWejsciowa = Console.ReadLine();
 
-                if(double.TryParse(wartoscWejsciowa,out wartosc))
+                if (double.TryParse(wartoscWejsciowa, out wartosc))
                 {
                     kolejka.Zapisz(wartosc);
                     continue;
                 }
                 break;
-
             }
-
-            Console.WriteLine("W naszej kolejce jest : ");
-
-            var suma = 0.0;
-            while (!kolejka.JestPusty)
-            {
-                suma += kolejka.Czytaj();
-            }
-            Console.WriteLine("\t\t" + kolejka.Czytaj());
-            Console.ReadLine();
         }
     }
+
 }
--- CsharpStrukturyGenertczne/2_KolekcjeGeneryczne/Program.cs
+++ CsharpStrukturyGenertczne/2_KolekcjeGeneryczne/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _2_KolekcjeGeneryczne
 {
@@ -10,6 +7,19 @@ namespace _2_KolekcjeGeneryczne
     {
         static void Main(string[] args)
         {
+            Queue<Pracownik> kolejka = new Queue<Pracownik>();
+            kolejka.Enqueue(new Pracownik { Imie = "Marcin", Nazwisko = "Nowak" });
+            kolejka.Enqueue(new Pracownik { Imie = "Tomek", Nazwisko = "Kozak" });
+            kolejka.Enqueue(new Pracownik { Imie = "Jacek", Nazwisko = "Zajac" });
+            kolejka.Enqueue(new Pracownik { Imie = "Ola", Nazwisko = "Kaczor" });
+
+            while (kolejka.Count > 0)
+            {
+                var pracownik = kolejka.Dequeue();
+                Console.WriteLine(pracownik.Imie + " " + pracownik.Nazwisko);
+            }
+            Console.ReadLine();
         }
+
     }
 }
